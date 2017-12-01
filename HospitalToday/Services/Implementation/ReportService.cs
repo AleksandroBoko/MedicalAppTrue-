8468e6956218e41be84a57164043e02e85d53166
Aleks B <aleksandroboko@gmail.com>
2017-12-01 17:47:56 +0200

    Fixed checking on null in the ReportService

--- HospitalToday/Services/Implementation/ReportService.cs
+++ HospitalToday/Services/Implementation/ReportService.cs
@@ -47,7 +47,7 @@ namespace HospitalToday.Services.Implementation
         public double GetReportTotalCost(int id)
         {
             var report = reportRep.GetItem(id);
-            if (report != null && report.Medicines != null)
+            if (report != null && report.Medicines.Any(x => x != null)) 
             {
                 return report.Medicines.Sum(x => x.Cost);
             }
